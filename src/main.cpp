fc283eefe599c9e0d0b4fbe539fffc0abeac6c08
FelixKrumme <125459395+FelixKrumme@users.noreply.github.com>
2024-06-06 07:08:49 +0200

    init centring motor

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,81 @@
 #include <Arduino.h>
+// #include "serial_communication.h"
+#include "motor_control.h"
 
 // Put here Interupt Service Routines for Endtriggers
 
 // put function declarations here:
 int myFunction(int, int);
+StepperMotor motor_big_centring_left = StepperMotor(motor_group_big_centring, platform_front_left, 12, 9, 8, 2);
+StepperMotor motor_big_centring_right = StepperMotor(motor_group_big_centring, platform_front_right, 12, 9, 8, 2);
 
-void setup() {
+StepperGroup group_big_centring = StepperGroup(1, 1000, HIGH);
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial)
+  {
+  };
+  Serial.println("Serial Communication started..");
+  pinMode(12, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  group_big_centring.addMotor(&motor_big_centring_left);
+  group_big_centring.addMotor(&motor_big_centring_right);
+
+
   // put your setup code here, to run once:
-
-
 }
 
-void loop() {
+void loop()
+{
+  // digitalWrite(12, HIGH);
+  // stepper1.moveByStepsBlocking(400, 5000);
+  // digitalWrite(12, LOW);
+  // stepper1.moveByStepsBlocking(800, 4000);
+  // delay(1000);
+
+  // group1.moveGroupByRotations(10, HIGH);
+
+  // group1.moveGroupByRotations(10, LOW);
+
+
+  // digitalWrite(17, HIGH);
+  // digitalWrite(25, HIGH);
+
+
+  // for (unsigned int i = 0; i < 800; i++)
+  // {
+  //   digitalWrite(18, HIGH);
+  //   delayMicroseconds(800);
+  //   digitalWrite(18, LOW);
+  //   delayMicroseconds(800);
+  // }
+  // digitalWrite(25, LOW);
+
+
+  // for (int i = 0; i < 800; i++)
+  // {
+  //   digitalWrite(18, HIGH);
+  //   delayMicroseconds(500);
+  //   digitalWrite(18, LOW);
+  //   delayMicroseconds(500);
+  // }
+  // digitalWrite(17, LOW);
+
   // put your main code here, to run repeatedly:
-  
-
-
+  // String* message = checkForMessage();
+  // if (message != nullptr) {
+  //     // Use the message...
+  //     delete message;  // Don't forget to delete it!
+  // }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
